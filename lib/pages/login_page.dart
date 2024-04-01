7d8c96c04a4d5080a76fa6a9ae436bba69461f91
its-kios09 <legacyitsolution@gmail.com>
2024-04-02 02:36:20 +0300

    (feat core) added light and dark theme

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 25),
+            Text("Poultry Guard Gen")
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: const Color(0xFF00473F),
+    secondary: Colors.grey.shade100,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade700
+  ),
+);
