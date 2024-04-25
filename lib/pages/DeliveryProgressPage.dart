614965ab5fa01d775aed10a2566df8eebd8dad66
its-kios09 <legacyitsolution@gmail.com>
2024-04-25 22:50:02 +0300

    (feat) delivery page

--- lib/pages/DeliveryProgressPage.dart
+++ lib/pages/DeliveryProgressPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:poultryguard/widgets/custom_receipt.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DeliveryProgressPage extends StatelessWidget {
   const DeliveryProgressPage({super.key});
@@ -16,38 +17,34 @@ class DeliveryProgressPage extends StatelessWidget {
       ),
       bottomNavigationBar: _buildBottomNavBar(context),
       body: Column(
-        children: [
-          CustomReceipt()
-        ],
+        children: [CustomReceipt()],
       ),
     );
   }
-  Widget _buildBottomNavBar(BuildContext context){
+
+  Widget _buildBottomNavBar(BuildContext context) {
     return Container(
       height: 100,
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40)
-        )
-      ),
-      padding: const EdgeInsets.all(25),
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40))),
+      padding: EdgeInsets.all(25),
       child: Row(
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.background,
-              shape: BoxShape.circle
-            ),
-            padding: const EdgeInsets.all(25),
+                color: Theme.of(context).colorScheme.background,
+                shape: BoxShape.circle),
             child: IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: const Icon(Icons.person),
             ),
           ),
-          const SizedBox(width: 10,),
-           Column(
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
@@ -55,16 +52,14 @@ class DeliveryProgressPage extends StatelessWidget {
                 style: TextStyle(
                     fontFamily: "JosefinSans",
                     fontSize: 18,
-                  color: Theme.of(context).colorScheme.primary
-                ),
+                    color: Theme.of(context).colorScheme.primary),
               ),
               Text(
-                "Poultry Guard Driver",
+                "Driver",
                 style: TextStyle(
                     fontFamily: "JosefinSans",
                     fontSize: 18,
-                    color: Theme.of(context).colorScheme.primary
-                ),
+                    color: Theme.of(context).colorScheme.primary),
               ),
             ],
           ),
@@ -74,23 +69,26 @@ class DeliveryProgressPage extends StatelessWidget {
               Container(
                 decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.background,
-                    shape: BoxShape.circle
-                ),
+                    shape: BoxShape.circle),
                 child: IconButton(
-                  onPressed: (){},
+                  onPressed: () {
+                    _sendMessage();
+                  },
                   icon: const Icon(Icons.message),
                   color: Colors.blueGrey,
                 ),
               ),
-              const SizedBox(width: 10,),
+              const SizedBox(
+                width: 10,
+              ),
               Container(
                 decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.background,
-                    shape: BoxShape.circle
-                ),
-                padding: const EdgeInsets.all(25),
+                    shape: BoxShape.circle),
                 child: IconButton(
-                  onPressed: (){},
+                  onPressed: () {
+                    _makePhoneCall('+25479166087');
+                  },
                   icon: const Icon(Icons.call),
                   color: Colors.green,
                 ),
@@ -100,5 +98,23 @@ class DeliveryProgressPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _makePhoneCall(String phoneNumber) async {
+    final url = 'tel:$phoneNumber';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _sendMessage() async {
+    final url = Uri.parse('sms:+25479166087');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
